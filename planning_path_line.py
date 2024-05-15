ea13d812027b6567030478a80525714772984ebd
JEESUNGSO <plzinseoul@gmail.com>
2024-05-15 16:15:15 +0900

    line planning update

--- planning_path_line.py
+++ planning_path_line.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 def get_path_line(cx, cy, gx, gy):
-    lx = np.arange(cx, gx, 0.5, dtype=float)
-    ly = (cy-gy)/(cx-gx) * (lx-cx) + cy
+    theta = np.arctan2(gy-cy, gx-cx)
+    l = np.arange(0, np.linalg.norm(np.array([gx-cx, gy-cy])), 0.5, dtype=float)
+    lx = l * np.cos(theta) + cx
+    ly = l * np.sin(theta) + cy
     return lx, ly
